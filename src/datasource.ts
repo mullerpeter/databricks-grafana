56708a35fe9f8414db8bdea4b2c3e1be8a747fd9
Peter Müller <6986867+mullerpeter@users.noreply.github.com>
2022-11-21 22:57:32 +0100

    :sparkles: feat: add support for custom variables

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,16 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { MyDataSourceOptions, MyQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+  }
+  applyTemplateVariables(query: MyQuery) {
+    const templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      rawSqlQuery: query.rawSqlQuery ? templateSrv.replace(query.rawSqlQuery) : ''
+    };
   }
 }
