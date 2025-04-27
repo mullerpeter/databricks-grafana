234d007d4d20cbbbc838bc18c818087895a09829
Peter Müller <6986867+mullerpeter@users.noreply.github.com>
2025-04-28 01:31:58 +0200

    :recycle: Refactor: forward pass-through oauth token via context (#117)

--- pkg/integrations/oauth_pass_trough.go
+++ pkg/integrations/oauth_pass_trough.go
@@ -2,17 +2,15 @@ package integrations
 
 import (
 	"fmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"net/http"
 	"sync"
 )
 
+// TokenStorage is a thread-safe storage for the OAuth pass-through token. Token Storage is needed as retries may not have the token in the context.
 type TokenStorage struct {
 	mu    sync.RWMutex
 	token string
-}
-
-func NewTokenStorage(initialToken string) *TokenStorage {
-	return &TokenStorage{token: initialToken}
 }
 
 func (ts *TokenStorage) Get() string {
@@ -34,14 +32,29 @@ type OAuthPassThroughAuthenticator struct {
 	tokenStorage *TokenStorage
 }
 
-func NewOAuthPassThroughAuthenticator(ts *TokenStorage) *OAuthPassThroughAuthenticator {
-	return &OAuthPassThroughAuthenticator{tokenStorage: ts}
+func NewOAuthPassThroughAuthenticator() *OAuthPassThroughAuthenticator {
+	return &OAuthPassThroughAuthenticator{
+		tokenStorage: &TokenStorage{},
+	}
 }
 
 func (a *OAuthPassThroughAuthenticator) Authenticate(r *http.Request) error {
-	if a.tokenStorage.Get() == "" {
-		return fmt.Errorf("OAuth pass-through token is empty")
+	tokenValue := r.Context().Value("pass_through_oauth_token")
+	token, ok := tokenValue.(string)
+	if !ok || token == "" {
+		// If the token is not in the context, check the token storage as a fallback
+		if a.tokenStorage.Get() != "" {
+			token = a.tokenStorage.Get()
+		} else {
+			return fmt.Errorf("OAuth pass-through token is missing or not a string")
+		}
 	}
-	r.Header.Set("Authorization", a.tokenStorage.Get())
+
+	if token != a.tokenStorage.Get() {
+		a.tokenStorage.Update(token)
+		log.DefaultLogger.Debug("Updating OAuth pass-through token")
+	}
+
+	r.Header.Set("Authorization", token)
 	return nil
 }
